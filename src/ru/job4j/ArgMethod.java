f52503f0a87117d5b8c7923f5495849f0eb43139
daidb <davidbilalov94@mail.ru>
2021-06-21 15:31:18 +0300

    4.1. Аргументы [#295521 #169449]

--- src/ru/job4j/ArgMethod.java
+++ src/ru/job4j/ArgMethod.java
@@ -1,18 +1,16 @@
 package ru.job4j;
 
 public class ArgMethod {
-    public static void hello(String name) {
+    public static void hello(String name, int n) {
         System.out.println("Hello, " + name);
+        System.out.println("Number, " + n);
     }
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
 
-        ArgMethod.hello(name);
+        ArgMethod.hello(name,age);
 
-        ArgMethod.hello(name);
-
-        ArgMethod.hello(name);
     }
 }
