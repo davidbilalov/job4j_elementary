4c1f3c8530f81e44e51971b159c44829784a8921
daidb <davidbilalov94@mail.ru>
2021-06-21 15:54:17 +0300

    5. Конвертер валюты [#41583]

---
+++ src/Converter.java
@@ -0,0 +1,18 @@
+public class Converter {
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
+        return rsl;
+    }
+
+    public static int rubleToDollar(int value) {
+        int rsl = value / 60; /* формула перевода рублей в доллары. */
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("140 rubles are " + dollar + " dollar.");
+    }
+}
