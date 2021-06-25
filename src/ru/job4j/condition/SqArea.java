f35a04b2977d66cf0c52b298a8efb472f58e8941
daidb <davidbilalov94@mail.ru>
2021-06-25 21:00:36 +0300

    4.5. Отладка программы в IDEA [#159464]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,14 +1,10 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, double k) {
-        double h = p / (2 * (k + 1));
-        double l = h * k;
-        return l * h;
-    }
-
-    public static void main(String[] args) {
-        double result1 = ru.job4j.SqArea.square(9, 2);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 }
--- src/ru/job4j/condition/SqAreaTest.java
+++ src/ru/job4j/condition/SqAreaTest.java
@@ -1,33 +1,25 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
 import org.junit.Test;
 
 public class SqAreaTest {
     @Test
-    public void whenP6K2Square2() {
-        int expected = 2;
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
+        int k = 2;
         int p = 6;
-        double k = 2;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void whenP3K2Square2() {
-        double expected = 0.5;
-        int p = 3;
-        double k = 2;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void whenP9K2Square2() {
-        double expected = 4.5;
-        int p = 9;
-        double k = 2;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        int expected = 2;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
 }
