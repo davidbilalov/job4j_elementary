bdaaf0b98dcba05889105ef1b3751e2e0348fbfc
daidb <davidbilalov94@mail.ru>
2021-06-26 21:39:22 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefenition.java
+++ src/ru/job4j/array/ArrayDefenition.java
@@ -3,8 +3,15 @@ package ru.job4j.array;
 public class ArrayDefenition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        String[] surname = new String[100500];
+        String[] surname = new String[4];
         float[] prices = new float[40];
+        surname[0] = "0";
+        surname[1] = "1";
+        surname[2] = "2";
+        surname[3] = "3";
+        for (int i = 0; i < surname.length; i++) {
+            System.out.println(surname[i]);
+        }
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surname.length);
         System.out.println("Размер массива равен: " + prices.length);
