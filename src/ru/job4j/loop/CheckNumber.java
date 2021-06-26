63301c66f16690d64fc9c3d21d2b3b0daa120d9f
daidb <davidbilalov94@mail.ru>
2021-06-26 18:21:04 +0300

    5.5. Простое число [#171694 #171525]

--- src/ru/job4j/loop/CheckNumber.java
+++ src/ru/job4j/loop/CheckNumber.java
@@ -2,14 +2,13 @@ package ru.job4j.loop;
 
 public class CheckNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number != 1) {
+        boolean prime = number > 1;
             for (int i = 2; i <= Math.sqrt(number); i++) {
                 if (number % i == 0) {
                     return false;
                 }
             }
-        }
+
         return prime;
     }
 
