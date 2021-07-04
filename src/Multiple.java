2baeaf2a606230c237158147bab200a5b4759a6b
daidb <davidbilalov94@mail.ru>
2021-07-05 00:54:46 +0300

    3.3. Разбор первой программы. [#159033 #168830]

--- src/Multiple.java
+++ src/Multiple.java
@@ -2,11 +2,11 @@ public class Multiple {
     public static void main(String[] args) {
         System.out.println("1 * 2 = " + 1 * 2);
         System.out.println("1 * 3 = " + 1 * 3);
-        System.out.println("1 * 3 = " + 1 * 4);
-        System.out.println("1 * 3 = " + 1 * 5);
-        System.out.println("1 * 3 = " + 1 * 6);
-        System.out.println("1 * 3 = " + 1 * 7);
-        System.out.println("1 * 3 = " + 1 * 8);
+        System.out.println("1 * 4 = " + 1 * 4);
+        System.out.println("1 * 5 = " + 1 * 5);
+        System.out.println("1 * 6 = " + 1 * 6);
+        System.out.println("1 * 7 = " + 1 * 7);
+        System.out.println("1 * 8 = " + 1 * 8);
         System.out.println("1 * 9 = " + 1 * 9);
     }
 }
